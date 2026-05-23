6a6eff6518579e8d803e4c6d48030b364de3a711
unknown <niyomubyeyi150@gmail.com>
2026-05-23 23:55:35 +0200

    Question4 about School Attendance

---
+++ Question4.js
@@ -0,0 +1,22 @@
+// School Attendance
+let attendance = 92;
+//Check if attendance is 90 or more.
+if( attendance ===90){
+    console.log("Attendance is 90 ");
+}else if (attendance >90){
+    console.log ("attendance is more");
+}else{
+    console.log("Attendance is bellow");
+}
+
+// 2. Check if attendance is below 75
+if (attendance < 75) {
+    console.log("Attendance is below 75.");
+}
+
+// 3. Check if attendance is NOT exactly 100
+if (attendance !== 100) {
+    console.log("Attendance is NOT exactly 100.");
+}else{
+    console.log("attendance is exactly 100")
+}
