f964b4072a1fae4d117424821f00819f2dd9ceb3
unknown <niyomubyeyi150@gmail.com>
2026-05-24 20:03:38 +0200

    Question 6 about car speed monitoring

---
+++ Question13.js
@@ -0,0 +1,11 @@
+// Phone Battery
+let battery = 15 ;
+// 1. Check if the battery is below 20.
+if (battery < 20 ){
+    console.log("Battery is low please charge");
+}
+// 2. Check if the battery is exactly 100.
+if (battery === 100){
+    console.log("Full battery");
+}
+
--- Question6.js
+++ Question6.js
@@ -5,6 +5,10 @@ if (speed > 80){
     console.log ("slow down your speed is high");
 }else if (speed < 30) {
     console.log("Alert! your speed is too low");
-}else{
-    console.log("Safe speed zone");
+}else if (speed >= 30 && speed <= 80) {
+    console.log("The speed is maximum");
+}else {
+    console.log(" speed is minimum");
 }
+    
+
