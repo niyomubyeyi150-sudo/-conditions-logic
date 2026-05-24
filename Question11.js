01479fbc32ffaf679baede3d51b6e0577599429e
unknown <niyomubyeyi150@gmail.com>
2026-05-24 19:37:03 +0200

    Question11 about Mobile Data Bundles

--- Question11.js
+++ Question11.js
@@ -1,2 +1,16 @@
 // Mobile Data Bundle
-let dataBalance = 500 ;
+let dataBalance = 50 ;
+if (dataBalance > 1000){
+    console.log("Data is above 1000 mbs");
+}
+// 2. Check if data is below 100.
+  if (dataBalance < 100){
+    console.log("Data is below 100 mbs");
+}
+// 3. Check if data is NOT equal to 0.
+ if (dataBalance !== 0){
+    console.log("you have remaining data");
+}else{
+    console.log("no remainining data");
+
+}
