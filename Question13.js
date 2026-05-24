fdd7fc251414049cfc72e1234cab347a1c533aa6
unknown <niyomubyeyi150@gmail.com>
2026-05-24 20:26:31 +0200

    Question 13 about phone battery

--- Question13.js
+++ Question13.js
@@ -1,11 +1,16 @@
 // Phone Battery
-let battery = 15 ;
+let battery = 15;
 // 1. Check if the battery is below 20.
 if (battery < 20 ){
     console.log("Battery is low please charge");
 }
 // 2. Check if the battery is exactly 100.
-if (battery === 100){
+ if (battery === 100){
     console.log("Full battery");
 }
-
+// 3. Check if the battery is NOT 0.
+ else if (battery !== 0){
+    console.log("Phone is on  ");
+}else {
+    console.log("Shutdown");
+}
