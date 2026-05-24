82906c124a617407a7a70639e04d651ce5f5d598
unknown <niyomubyeyi150@gmail.com>
2026-05-24 17:00:13 +0200

    Question7 about electricity usage some changes

---
+++ Question11.js
@@ -0,0 +1,2 @@
+// Mobile Data Bundle
+let dataBalance = 500 ;
--- Question8.js
+++ Question8.js
@@ -3,8 +3,12 @@
 let unitsUsed = 140;
 if (unitsUsed > 100){
     console.log("High electricity usage");
-}else if (unitsUsed <= 50 && unitsUsed !== 0){ 
-    console.log("lower usage of electricity , Electricity is available");
+}
+if (unitsUsed <= 50){ 
+    console.log("lower usage of electricity");
+}
+ if (unitsUsed !== 0){
+    console.log("Electricity is available");
 }else{
     console.log("electricity is not available"); 
 }
