c9a899ce3e5d8be6b5417ac650ac0da5b117599c
unknown <niyomubyeyi150@gmail.com>
2026-05-25 00:12:41 +0200

    Question 24 about Weather alert

--- Question24.js
+++ Question24.js
@@ -7,3 +7,16 @@ if (temperature < 10){
 }else{
     console.log("The temperature is above");
 }
+// 2. Check if it is raining.
+if (isRaining === true){
+    console.log("It is raining");
+} else{
+    console.log("It is not raining");
+}
+// 3. Check if a cold weather alert should be shown.
+if (temperature < 10 && isRaining){
+    console.log("A cold weather alert should be down");
+}else {
+  console.log("A cold weather alert should not be down");  
+}
+
