031e7e8e4bd5181beca167e0f0d631b5f420cede
unknown <niyomubyeyi150@gmail.com>
2026-05-24 16:06:31 +0200

    Question9 about Employee salary

---
+++ Question9.js
@@ -0,0 +1,14 @@
+//  Employee Salary
+
+let salary= 350000;
+
+if (salary > 300000){
+    console.log (" your salary is greater than 300000  ");
+}else if ( salary < 100000){
+    console.log("your salary is less than 100000");
+
+} if(salary == 500000){
+    console.log("your salary is exactly 500000");
+}else {
+    console.log ( "Less salary");
+}
