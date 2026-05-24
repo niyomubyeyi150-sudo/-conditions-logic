379d32ba11b6439c4ec31abcdd3f236c75aaa0a9
unknown <niyomubyeyi150@gmail.com>
2026-05-24 23:45:17 +0200

    Question 23 about Traffic Light System

---
+++ Question23.js
@@ -0,0 +1,15 @@
+//  Traffic Light System
+let lightColor = "green";
+switch (lightColor){
+    case "red":
+        console.log("Bus stop ! the the color is red");
+        break;
+    case "green":
+        console.log(" people Go! The light is Green") ; 
+        break;
+    case"yellow":
+    console.log("The light is not yellow ");
+    break;
+    default:
+        console.log("The light is not yellow");
+}
