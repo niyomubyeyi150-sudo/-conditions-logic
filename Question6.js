73d1a8de727941f2a6e4422a407d57ffeddd7306
unknown <niyomubyeyi150@gmail.com>
2026-05-24 15:23:09 +0200

    Question6 about car speed monitoring

---
+++ Question6.js
@@ -0,0 +1,10 @@
+//  Car Speed Monitoring
+let speed = 65;
+//Check if speed is above 80.
+if (speed > 80){
+    console.log ("slow down your speed is high");
+}else if (speed < 30) {
+    console.log("Alert! your speed is too low");
+}else{
+    console.log("Safe speed zone");
+}
