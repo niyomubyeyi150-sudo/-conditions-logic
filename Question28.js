2fc6f7387319fc5c50a2fe64317c5bcffbb8d9f2
unknown <niyomubyeyi150@gmail.com>
2026-05-25 14:54:10 +0200

    Question 28 about Employee login System

---
+++ Question28.js
@@ -0,0 +1,21 @@
+// Employee Login System
+let username = "admin";
+let password = "12345";
+// 1. Check if username equals "admin".
+if (username === "admin"){
+    console.log("username equal 'admin'");
+}else{
+    console.log("username does not equal'admin'");
+}
+// 2. Check if the password equals "12345".
+if(password === "12345"){
+    console.log("The password equals '12345' ");
+}else{
+  console.log("The password is not  equals '12345' ");  
+}
+// 3. Check if both login details are correct.
+if (username === "admin" && password === "12345"){
+    console.log("both login details are correct");
+}else{
+    console.log("both login details are not  correct"); 
+}
